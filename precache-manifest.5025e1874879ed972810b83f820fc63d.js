abea4e8b86128aefbf3b498f82dc53182ca58cd8
fernando-pineda <fernando.pineda2199@gmail.com>
2020-07-19 20:06:08 -0500

    Updates

---
+++ precache-manifest.5025e1874879ed972810b83f820fc63d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001a5a9adaee926b75a17dbc9b413030",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a764ddb8db85fb69bdfb",
+    "url": "/my-portfolio/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2c8d46584656a74f75aa",
+    "url": "/my-portfolio/static/css/main.86af0d8a.chunk.css"
+  },
+  {
+    "revision": "a764ddb8db85fb69bdfb",
+    "url": "/my-portfolio/static/js/2.c420aa64.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/my-portfolio/static/js/2.c420aa64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8d46584656a74f75aa",
+    "url": "/my-portfolio/static/js/main.27078384.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "87b9d4da14294e791717062d5826cb49",
+    "url": "/my-portfolio/static/media/CV_Preview.87b9d4da.png"
+  },
+  {
+    "revision": "b1b551d25d4a7d6c5013e9dfa789fe62",
+    "url": "/my-portfolio/static/media/Fernando_Pineda_CV.b1b551d2.pdf"
+  },
+  {
+    "revision": "39e416ddd643c785551b55942963d06f",
+    "url": "/my-portfolio/static/media/Fernando_Pineda_Cover_Letter.39e416dd.docx"
+  },
+  {
+    "revision": "db3db5db72a19da92330e9db79714250",
+    "url": "/my-portfolio/static/media/aboutPhoto.db3db5db.jpg"
+  },
+  {
+    "revision": "2a2051ec11de422edf8f8586c97c2825",
+    "url": "/my-portfolio/static/media/movienator_1.2a2051ec.jpeg"
+  },
+  {
+    "revision": "985ce36cf48a48b1831fef5e2eb3db8d",
+    "url": "/my-portfolio/static/media/movienator_3.985ce36c.jpeg"
+  },
+  {
+    "revision": "8568c4dccb8ffa741b2de55ed109c6ae",
+    "url": "/my-portfolio/static/media/navPhoto.8568c4dc.jpg"
+  },
+  {
+    "revision": "a427a40ee13317250d83637b60da1532",
+    "url": "/my-portfolio/static/media/portfolio.a427a40e.png"
+  },
+  {
+    "revision": "f3fd99862458ef4929b1cb14df03db48",
+    "url": "/my-portfolio/static/media/videogameDataSearch.f3fd9986.png"
+  }
+]);
